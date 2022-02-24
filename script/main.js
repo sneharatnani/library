bc88325ae39ad3ca3b9935b093d459e8059cc615
sneharatnani <sneharatnani5@gmail.com>
2022-02-24 17:13:49 +0530

    added footer and media queries

--- script/main.js
+++ script/main.js
@@ -1,22 +1,32 @@
 const modal = document.querySelector('.modal');
 const newBookBtn = document.querySelector('.new-book');
+const overlay = document.querySelector('.overlay');
+const cancelBtn = document.querySelector('.cancel');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const page = document.querySelector('#number');
+const checkBox = document.querySelector('#status');
+const submitBtn = document.querySelector('#submit');
+// to display books
+const mainContainer = document.querySelector('.books');
+
+// open form to add new book
 newBookBtn.addEventListener('click', () => {
     modal.removeAttribute('id', 'popup');
     overlay.setAttribute('id', 'overlay');
 });
-
-const cancelBtn = document.querySelector('.cancel');
+// close the new book form
 cancelBtn.addEventListener('click', () => {
     modal.setAttribute('id', 'popup');
     overlay.removeAttribute('id', 'overlay');
-})
+});
 
-function Book() {
-    this.title = title.value;
-    this.author = author.value;
-    this.page = page.value;
-    this.status = function () {
-        if (checkBox.checked) {
+function Book(title, author, page, cbStatus) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.showStatus = function () {
+        if (cbStatus) {
             return 'read';
         }
         else {
@@ -24,28 +34,30 @@ function Book() {
         }
     };
 }
-
-let myLibrary = [];
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const page = document.querySelector('#number');
-const checkBox = document.querySelector('#status');
-const submitBtn = document.querySelector('#submit');
-
+// save the instances of Book
+const myLibrary = [];
 submitBtn.addEventListener('click', () => {
     if (title.value !== '' && author.value !== '' && page.value !== '') {
-        myLibrary.push(new Book());
-        addBookToLibrary(myLibrary[myLibrary.length - 1]);
+        myLibrary.push(new Book(title.value, author.value, page.value, checkBox.checked));
+        addBookToLibrary(myLibrary);
         title.value = '';
         author.value = '';
         page.value = '';
+        checkBox.checked = false;
         modal.setAttribute('id', 'popup');
         overlay.removeAttribute('id', 'overlay');
     }
 });
 
-function addBookToLibrary(obj) {
-    const mainContainer = document.querySelector('.books');
+// iterate over array and display the book
+function addBookToLibrary(arr) {
+    mainContainer.textContent = '';
+    for (let i = 0; i < arr.length; i++) {
+        makeBook(arr[i]);
+    };
+}
+
+function makeBook(obj) {
     const bookContainer = document.createElement('div');
     const title = document.createElement('p');
     const author = document.createElement('p');
@@ -55,28 +67,38 @@ function addBookToLibrary(obj) {
     title.textContent = obj.title;
     author.textContent = obj.author;
     page.textContent = obj.page;
-    statusBtn.textContent = obj.status();
+    statusBtn.textContent = obj.showStatus();
     removeBtn.textContent = 'remove';
     removeBtn.setAttribute('class', 'remove');
     bookContainer.append(title, author, page, statusBtn, removeBtn);
     mainContainer.appendChild(bookContainer);
     bookContainer.classList.add('book');
-
+    // remove book
     removeBtn.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(obj), 1);
         mainContainer.removeChild(bookContainer);
     });
-
+    // update the reading status
     statusBtn.addEventListener('click', () => {
-        if (statusBtn.textContent === 'read') {
+        if (obj.showStatus() === 'read') {
             statusBtn.textContent = 'not read';
+            myLibrary[myLibrary.indexOf(obj)].showStatus = function () {
+                return 'not read';
+            };
         }
         else {
             statusBtn.textContent = 'read';
+            myLibrary[myLibrary.indexOf(obj)].showStatus = function () {
+                return 'read';
+            };
         }
     });
 }
-// overlay
-const overlay = document.querySelector('.overlay');
+
+// current year
+let currentYear = new Date().getFullYear();
+document.querySelector('.year').textContent = currentYear;
+
 
 
 
